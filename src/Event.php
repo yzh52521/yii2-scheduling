f948dd0b701c41c7dd17b6efdfe7ba307e4d727c
yuanzhihai <396751927@qq.com>
2023-08-25 12:48:09 +0800

    update

--- src/Event.php
+++ src/Event.php
@@ -142,9 +142,7 @@ class Event extends Component
      */
     protected function runCommandInForeground(Application $app)
     {
-        (new Process(
-            trim($this->buildCommand(), '& '), dirname($app->request->getScriptFile()), null, null, null
-        ))->run();
+        Process::fromShellCommandline(trim($this->buildCommand(), '& '), dirname($app->request->getScriptFile()), null, null, null)->run();
         $this->callAfterCallbacks($app);
     }
 
